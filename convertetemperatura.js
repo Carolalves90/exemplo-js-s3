755f3598780f1144d1452619f680c2d6ff124350
carolalves <prof.carolalves90@gmail.com>
2022-04-13 20:38:31 -0300

    conversor resolvido

--- convertetemperatura.js
+++ convertetemperatura.js
@@ -1,1 +1,9 @@
 //Crie uma função que recebe uma temperatura em graus celsius retorna o valor convertido em Fahrenheit.
+
+function converterTemperatura(grausCelsius){
+    const grausFahrenheit = (grausCelsius * 9/5) + 32
+    return `A temperatura convertida é ${grausFahrenheit}ºF`
+}
+
+const resultado = converterTemperatura(19)
+console.log(resultado)
