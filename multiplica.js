8c5c1bad2bf61d73b7c46ebd50dc87de90b16a6e
carolalves <prof.carolalves90@gmail.com>
2022-04-13 20:45:45 -0300

    multiplica  resolvido

--- multiplica.js
+++ multiplica.js
@@ -1,1 +1,8 @@
 //Crie uma função que recebe 2 parâmetros e retorna a multiplicação entre else.
+
+function multiplica(n1, n2){
+    const resultado = n1 * n2
+    return (resultado)
+}
+
+console.log(multiplica(9,4))
