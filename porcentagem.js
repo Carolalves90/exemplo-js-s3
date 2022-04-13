ad4cc4a539b141ccb67426904f0cf3f117777485
carolalves <prof.carolalves90@gmail.com>
2022-04-13 20:58:04 -0300

    porcentagem resolvido

--- porcentagem.js
+++ porcentagem.js
@@ -1,1 +1,8 @@
 //Crie uma função que calcula 5% de desconto retornando o valor do desconto.
+
+function descontoDeCincoPorCento (valor){
+    const resultado = valor * (5/100)
+    return `Seu desconto é de ${resultado} reais`
+}
+
+console.log(descontoDeCincoPorCento(500))
