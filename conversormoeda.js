dc73152db9d51ef29a3e64caabc87e76d770cb45
carolalves <prof.carolalves90@gmail.com>
2022-04-13 21:11:40 -0300

    converso resolvido

--- conversormoeda.js
+++ conversormoeda.js
@@ -1,1 +1,9 @@
 //Crie uma função que irá converter uma quantia de real para dolar utilizando a cotação do dia.
+
+function conversorMoeda (valorReal){
+    const valorDolar = 4.68
+    const resultado = valorReal / valorDolar
+    return `${resultado.toFixed(2)} dólares`
+}
+
+console.log(conversorMoeda(5))
