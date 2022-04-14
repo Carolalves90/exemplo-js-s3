523aca86f56b41abbb125a346ab1b813e2de1e6c
carolalves <prof.carolalves90@gmail.com>
2022-04-13 21:34:38 -0300

    calculadora resolvido

--- calculadora.js
+++ calculadora.js
@@ -2,6 +2,26 @@
 
 // a) Deverá ser possível escolher uma operação aritimética em forma de string: 
 // 'soma', 'multiplicacao', 'divisao' e 'subtracao'
-
 // b) Deverá ser possível passar 2 números para a operação escolhida
 // c) Deverá retornar o resultado e imprimir no console
+
+function calcula(operador, n1, n2){
+    let resultado
+    if (operador === 'soma'){
+        resultado = n1 + n2
+        return resultado
+    } else if(operador === 'multiplicação'){
+        resultado = n1 * n2
+        return resultado
+    } else if(operador === 'divisão'){
+        resultado = n1 / n2
+        return resultado
+    } else if (operador === 'subtração'){
+        resultado = n1 - n2
+        return resultado
+    } else {
+        return `${operador} é invalido`
+    }
+}
+
+console.log(calcula('porcentagem',3,5))
